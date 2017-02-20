3dc094fab6b5ce5a7730f58ee00f763dd1b6c104
meist3r-ed <g.gaiax@gmail.com>
2017-02-20 20:39:26 -0300

    Services page finished :)

--- script/services.js
+++ script/services.js
@@ -25,7 +25,26 @@ function services_button_3_text(){
     + "Intranet, Internet e canais de comunicação. "
     + "<hr></hr> "
     + "Garantia da disponibilidade e capacidade do ambiente através "
-    + "de monitoramento proativo e proposta de solução de gargalos."
+    + "de monitoramento proativo e proposta de solução de gargalos. "
+    return text;
+}
+
+function services_button_4_text_1(){
+    var text = "Desenvolvimentos de projetos sob medida para a "
+    + "adequação ou expansão da infraestrutura de TI às "
+    + "necessidades de negócio da empresa. Balançeamento de "
+    + "soluções de acordo com a capacidade e a atenção aos "
+    + "pontos críticos de negócio. "
+    + "<hr></hr> "
+    return text;
+}
+
+function services_button_4_text_2(){
+    var text = "Desenvolvimentos de projetos de sistemas para todo "
+    + "tipo de necessidade e aplicação. Projetos especiais para "
+    + "internet e serviços móveis. Terceirização de desenvolvimento "
+    + "de sistemas e módulos customizados de acordo com as "
+    + "necessidades. ";
     return text;
 }
 
@@ -89,6 +108,12 @@ function services_select(num){
         case 4:
             bracket.style.marginLeft = "49%";
             button.innerHTML = "<span class=\"text_bold\">Projetos</span>";
+            title.innerHTML = "Projetos de Infraestrutura de TI";
+            body.innerHTML = services_button_4_text_1();
+            var title_2 = document.getElementById("content_text_title_2");
+            title_2.innerHTML = "Projetos de Sistemas";
+            var body_2 = document.getElementById("content_text_body_2");
+            body_2.innerHTML = services_button_4_text_2();
             break;
         default:
             break;
